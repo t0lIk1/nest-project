e36e856864af7d6230f340547b79aca889630824
t0lIk1 <anatolijkuzir@gmail.com>
2025-02-24 16:46:25 +0300

    feat: update roles

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@nestjs/common';
+import {CreateUserDto} from "../users/dto/create-user.dto";
+import {UsersService} from "../users/users.service";
+import {JwtService} from "@nestjs/jwt";
+
+@Injectable()
+export class AuthService {
+
+    constructor(private userService: UsersService, private jwtService: JwtService) {
+    }
+
+    async login(userDto: CreateUserDto) {
+    }
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.jwtService.sign(userDto);
+    }
+}
