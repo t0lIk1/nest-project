7448085f24193ddc5d183b22638792f52b6112b1
t0lIk1 <anatolijkuzir@gmail.com>
2025-02-24 11:17:18 +0300

    feat: connect DB

---
+++ server/src/users/users.model.ts
@@ -0,0 +1,26 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+
+interface UserCreationAttributes {
+    email: string;
+    password: string;
+}
+
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttributes> {
+
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    declare id: number;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @Column({type: DataType.STRING, allowNull: true, })
+    banReason: string;
+}
